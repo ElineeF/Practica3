7ebe11148cea811f7694f2e2040db944b785982d
ElineeF <en.freites.2022@alumnos.urjc.es>
2023-11-28 12:27:36 +0100

    Carpeta views y demás detalles

--- src/crearElementoService.js
+++ src/crearElementoService.js
@@ -7,7 +7,8 @@ router.post('/nuevaReceta', (req, res) => {
     res.render('receta', {
         image: req.body.image,
         rcpName: req.body.rcpName,
-        rcpIngredients: req.body.rcpIngredients
+        rcpIngredients: req.body.rcpIngredients,
+        rcpDescription: req.body.rcpDescription
     });
 });
 
