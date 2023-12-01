cbe5527ab0b55ee689f91df014a019bb231e1fb3
shania <s.manso.2022@alumnos.urjc.es>
2023-12-01 18:53:56 +0100

    reorganización de archivos y adición de funciones para página

--- src/crearElementoRouter.js
+++ src/crearElementoRouter.js
@@ -3,7 +3,7 @@ import * as crearElementoService from './crearElementoService.js';
 
 const router = express.Router();
 
-router.post('/nuevaReceta', (req, res) => {
+/*router.post('/nuevaReceta', (req, res) => {
 
     res.render('receta', {
         image: req.body.image,
@@ -19,14 +19,44 @@ router.post('/nuevaReceta', (req, res) => {
         dietRestrictions: req.body.dietRestrictions,
         celiac: req.body.celiac
     });
+});*/
+
+
+router.get('/', (req, res) => {
+    // Lógica para renderizar la página principal con la lista de recetas
+    res.render('index', { recipes: crearElementoService.getAllRecipes() });
+});
+  
+router.get('/crearElemento', (req, res) => {
+    // Renderizar la página de creación de recetas
+    res.render('crearElemento');
+});
+  
+router.post('/crearElemento', (req, res) => {
+    // Lógica para procesar el formulario y crear una nueva receta
+    const newRecipe = crearElementoService.createRecipe(req.body);
+    res.redirect(`/detalle/${newRecipe.id}`);
+});
+  
+router.get('/detalle/:id', (req, res) => {
+    // Lógica para renderizar la página de detalle de una receta por su ID
+    const recipe = crearElementoService.getRecipeById(req.params.id);
+    if (!recipe) {
+      res.status(404).send('Receta no encontrada');
+    } else {
+      res.render('detalle', { recipe });
+    }
 });
 
-/*Borrar elemento código base*/
-router.get('/post/:id/delete', (req, res) => {
-
-    crearElementoService.deletePost(req.params.id);
-
-    res.render('deleted_post');
-});
+router.post('/detalle/:id/delete', (req, res) => {
+    const recipeId = req.params.id;
+    const deleted = crearElementoService.deleteRecipeById(recipeId);
+  
+    if (!deleted) {
+      res.status(404).send('Receta no encontrada');
+    } else {
+      res.redirect('/');
+    }
+  });
 
 export default router;
--- src/crearElementoService.js
+++ src/crearElementoService.js
@@ -1,4 +1,46 @@
+const recipes = [];
 
-export function deletePost(id){
-    posts.delete(id);
+export function createRecipe(recipeData) {
+  // Lógica para validar y guardar la nueva receta
+  const newRecipe = {
+    id: recipes.length + 1,
+    rcpName: recipeData.rcpName,
+    rcpImage: recipeData.rcpImage,
+    rcpIngredients: recipeData.rcpIngredients.split('\n').map(line => line.trim()),
+    rcpDescription: recipeData.rcpDescription,
+    lactose: recipeData.lactose === 'on',
+    nuts: recipeData.nuts === 'on',
+    soya: recipeData.soya === 'on',
+    seafood: recipeData.seafood === 'on',
+    fish: recipeData.fish === 'on',
+    dietRestrictions: recipeData.dietRestrictions,
+  };
+
+  recipes.push(newRecipe);
+  return newRecipe;
 }
+
+export function getRecipeById(recipeId) {
+  // Lógica para obtener una receta por su ID
+  const recipe = recipes.find(recipe => recipe.id === Number(recipeId));
+
+  if (recipe) {
+    recipe.showDietRestriction = recipe.dietRestrictions !== null;
+  }
+
+  return recipe;
+}
+
+
+export function getAllRecipes() {
+    return recipes;
+  }
+
+export function deleteRecipeById(recipeId) {
+    const index = recipes.findIndex(recipe => recipe.id === Number(recipeId));
+    if (index !== -1) {
+      recipes.splice(index, 1);
+      return true; // receta eliminada 
+    }
+    return false; // receta no encontrada
+  }
